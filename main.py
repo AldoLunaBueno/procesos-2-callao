548ef6d21235cb8b56f0143686083d11f47ac296
AldoLunaBueno <albanovaphi7@gmail.com>
2025-07-09 17:20:03 -0500

    mostrar resultados en consola y corregir wqarning por valores duplicados en interpolación

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+from matplotlib.ticker import MaxNLocator
 from scipy.interpolate import interp1d
 from scipy.optimize import bisect
 
 import os
 import sys
 
+MUESTREO_APROXIMACION = 20
+
+
 def establecer_tkinter():
     os.environ["TCL_LIBRARY"] = os.path.join(sys.base_prefix, "tcl", "tcl8.6")
     os.environ["TK_LIBRARY"] = os.path.join(sys.base_prefix, "tcl", "tk8.6")
+
 
 def ingesta_datos():
     fase_oleica = pd.read_table("fase_oleica.txt", decimal=",")
     fase_propano = pd.read_table("fase_propano.txt", decimal=",")
     
     return fase_oleica, fase_propano
+
 
 def graficar(X_N, Y_N, M_acum, R_acum, E_acum, n):
     # Graficar X vs N y Y vs N
@@ -25,9 +31,16 @@ def graficar(X_N, Y_N, M_acum, R_acum, E_acum, n):
     for i in range(n):
         plt.plot([R_acum[i][0], E_acum[i][0]], [R_acum[i][1], E_acum[i][1]], "go-")
         plt.plot(M_acum[i][0], M_acum[i][1], "ro")
+        plt.text(R_acum[i][0] + 0.01, R_acum[i][1], f"$R_{i+1}$")
+        plt.text(E_acum[i][0] + 0.01, E_acum[i][1], f"$E_{i+1}$")
+        plt.text(M_acum[i][0] + 0.01, M_acum[i][1], f"$M_{i+1}$")
     plt.xlabel("Fracción molar")
     plt.ylabel("N")
+    plt.tick_params(axis='both', labelsize=6)
     plt.legend()
+    # Número de ticks en los ejes
+    plt.gca().xaxis.set_major_locator(MaxNLocator(nbins=12))
+    plt.gca().yaxis.set_major_locator(MaxNLocator(nbins=12))
 
     # Graficar X vs Y
     plt.subplot(2, 1, 2)
@@ -35,16 +48,22 @@ def graficar(X_N, Y_N, M_acum, R_acum, E_acum, n):
     plt.xlabel("X")
     plt.ylabel("Y")
     plt.plot([0, 1], [0, 1], "k:")
+    plt.tick_params(axis='both', labelsize=6)
     plt.legend()
+    # Número de ticks en los ejes
+    plt.gca().xaxis.set_major_locator(MaxNLocator(nbins=12))
+    plt.gca().yaxis.set_major_locator(MaxNLocator(nbins=12))
     
     # Mostrar y guardar el diagrama
     plt.tight_layout()
     os.makedirs("diagramas", exist_ok=True)
-    plt.savefig("diagramas/fig-1.png") 
+    plt.savefig("diagramas/fig-1.png")
     plt.show()
-    
+
+
 def calcular_s_prima(S):
     return S["masa"] / (1 + S["ns"])
+
 
 def calcular_M(F, S):
     s_ = calcular_s_prima(S)
@@ -52,6 +71,7 @@ def calcular_M(F, S):
     nm = (F["masa"] * F["nf"] + s_ * S["ns"]) / (F["masa"] + s_)
     
     return xm, nm
+
 
 def aproximar(M, X_N, Y_N):
     # interpolaciones lineales (por defecto)
@@ -61,11 +81,21 @@ def aproximar(M, X_N, Y_N):
     f_X_Y_Ne = lambda x: f_Y_Ne(f_X_Y(x))  # función composición
     
     def f(x):
-        cuerda = interp1d([x, f_X_Y(x)], [f_X_Nr(x), f_X_Y_Ne(x)], fill_value="extrapolate")
+        x1 = x
+        x2 = f_X_Y(x)
+        if np.isclose(x1, x2):
+            x2 += 1e-6  # o simplemente: return np.nan
+        cuerda = interp1d([x1, x2], [f_X_Nr(x), f_X_Y_Ne(x)], fill_value="extrapolate")
         nm_ = cuerda(M["xm"])
         return nm_ - M["nm"]
+    a = 0
+    b = 0
     
-    x_opt = bisect(f, 0.1, 0.9)
+    for b in np.linspace(0, 1, MUESTREO_APROXIMACION):
+        if f(a) * f(b) < 0:
+            break
+        a = b
+    x_opt = bisect(f, a, b)
     return x_opt, f_X_Nr(x_opt), f_X_Y(x_opt), f_X_Y_Ne(x_opt)
 
 
@@ -82,13 +112,15 @@ def liq_liq_n_etapas(n: int):
     
     # datos del problema
     temp = 98.3  # grados centígrados
-    F = {"masa": 100, "xf": 0.5, "nf": 0}
-    S = {"masa": 1500, "ys": 0.1, "ns": 49}
+    F = {"masa": 100, "xf": 0.5, "nf": 0}  # se actualiza en cada iteración
+    S = {"masa": 1500, "ys": 0.1, "ns": 49}  # constante en todas las iteraciones
     # A: aceite, B: propano, C: ac. oleico
     solvente = {"A": 0.018, "B": 0.98, "C": 0.002}
     M_acum = []
     R_acum = []
     E_acum = []
+    E_masa = []
+    y_acum = []
     for i in range(n):        
         Xm, Nm = calcular_M(F, S)
         s_ = calcular_s_prima(S)
@@ -97,21 +129,31 @@ def liq_liq_n_etapas(n: int):
         masa_r_prima = M["masa"] * (Y - M["xm"]) / (Y - X)
         masa_e_prima = M["masa"] * (M["xm"] - X) / (Y - X)
         masa_e = masa_e_prima * (1 + Ne)
-        # y = Y / (1 + Ne)
-        # x = X / (1 + Nr)
+        y = Y / (1 + Ne)
+        x = X / (1 + Nr)
         F = {"masa": masa_r_prima, "xf": X, "nf": Nr}  # R
         R = [X, Nr]
         E = [Y, Ne]
         M_acum.append([M["xm"], M["nm"]])
         R_acum.append(R)
         E_acum.append(E)
+        E_masa.append(masa_e)
+        y_acum.append(y)
 
+    # resultados en consola
+    print(f"Cantidad de etapas: {n}")
+    E_compuesto = sum(E_masa)
+    print(f"E compuesto: {E_compuesto}")
+    y_compuesto = (sum([e*y for e, y in zip(E_masa, y_acum)])) / E_compuesto
+    print(f"y compuesto: {y_compuesto}")
+    
+    # diagramas
     graficar(X_N, Y_N, M_acum, R_acum, E_acum, n)
     
   
 def main():
     establecer_tkinter()
-    liq_liq_n_etapas(2)
+    liq_liq_n_etapas(4)
 
 
 if __name__ == "__main__":
